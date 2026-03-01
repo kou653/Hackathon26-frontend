2add639f0a97a5a47628085eb89f09de03d4e9f7
Kouassi Roxane <rkouassi372@gmail.com>
2026-03-01 00:30:35 +0000

    Connexion & jeu

--- sdi25-master/src/screens/auth/AuthView.jsx
+++ sdi25-master/src/screens/auth/AuthView.jsx
@@ -62,12 +62,12 @@ export default function AuthView() {
             promouvoir les TIC.
           </p>
           <div className="flex justify-center gap-4 mt-11">
-            {/* <LinkButton
+            <LinkButton
               label="Connexion"
               type="button"
               route="/hackathon/auth/LogInView"
             />
-            <LinkButton
+            {/* <LinkButton
               label="Inscription"
               type="button"
               route="/hackathon/auth/SignInView"
